9e1695309ae26a709bed99576a51355b2b818254
Kent <kenth3@gmail.com>
2020-05-31 18:10:02 -0600

    converted to using env variables for app and docker-compose

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-rm ./lib/config/config.development.js
-mv ./lib/config/config.docker.js ./lib/config/config.development.js
+# The following 2 lines should not be necessary any more now that the config values exist as environment 
+# variables, and populated as part 
+# rm ./lib/config/config.development.js
+# mv ./lib/config/config.docker.js ./lib/config/config.development.js
 
 # upgrade the npm version joi will be installed correctly with the dependencies
 npm i npm
@@ -9,6 +11,8 @@ npm i npm
 # install dependencies for the server
 npm install
 
+# This shouldn't be necessary any more either, as we're already in 
+# the /app work directory, and the aurelia app is no longer part of the api container 
 # install dependencies for the aurelia front-end
-cd /app
-npm install
+# cd /app
+# npm install
